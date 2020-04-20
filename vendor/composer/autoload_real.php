15a8a792e86d8a2293020daf92fa56b2dee6b329
kaisa316 <490788241@qq.com>
2020-04-20 18:44:18 +0800

    laravel ide

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInit8955a797438ae7c8d1df8fc87820f5d2
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
