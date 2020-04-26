d9ecdc8231afc924663da04ec9d5fac68c8cab8d
kaisa316 <490788241@qq.com>
2020-04-26 18:58:26 +0800

    validate 不通过返回json格式

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\ForceJson;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -41,6 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\ForceJson::class,//强制json
         ],
     ];
 
